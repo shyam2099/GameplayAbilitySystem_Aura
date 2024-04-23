c0587aec3a981010b4e57f7304da77c2ad71f17e
Shyam <shyam.2099@gmail.com>
2024-04-23 15:49:51 +0530

    Updated character classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// -sam2099
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// -sam2099
+
+
+#include "Character/AuraEnemy.h"
+
